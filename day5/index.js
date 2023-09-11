fb85d762fa838d066be750ffa97d2f857ff73f31
Abhinandan Jain <abhinandanbansal123@gmail.com>
2023-09-11 21:51:13 +0530

    dom understand

---
+++ day5/index.js
@@ -0,0 +1,11 @@
+function setAll() {
+  let element1 = `<div> avengers endgame </div>`;
+
+  document.getElementById("container1").innerHTML = element1;
+
+  let element2 = React.createElement("div", null, "avengers endgame");
+
+  ReactDOM.render(element2, document.getElementById("container2"));
+}
+
+setInterval(setAll, 1000);
